28487d70cbcd2fbe175787b1153f969b0258f037
Ismail Moussaoui <imoussaoui@lacentrale.fr>
2021-11-22 23:11:41 +0100

    loop all the file using the manifest

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import os
 
 import utils
+from atlanapi.atlanutils import AtlanSourceFile
 from validate_atlan_source_file import validate_atlan_source_file as validate
 from create_atlan_dynamodb_entity import create_atlan_dynamodb_entity as create_table
 from create_atlan_columns import create_atlan_columns
@@ -9,18 +10,24 @@ from create_atlan_column_lineage import create_atlan_column_lineage
 import logging
 
 if __name__ == '__main__':
-    logging.basicConfig(stream=sys.stdout,
-                        level=logging.INFO,
-                        format='%(asctime)s %(message)s')
+    # setting up logger
+    utils.setup_logger('main_logger')
+    logger = logging.getLogger('main_logger')
+    logger.info("starting the job ...")
 
-    path="/github/workspace/docs/data-catalogue-index/test-cible-table.csv"
-    table_name = utils.get_table_name(path)
-    entity= "test"
-    description = "created by github action"
-    validate(path)
-    create_table(path, entity, description)
-    create_atlan_columns(path)
-    #create_atlan_column_lineage(path, "DynamoDb")
-
-
-  #/github/workspace/docs/
+    # load manifest
+    source_data = AtlanSourceFile(utils.get_manifest_path(), sep=",")
+    source_data.load_csv()
+    for index, row in source_data.assets_def.iterrows():
+        table = row['Table']
+        path = utils.get_path(table)
+        entity = row['Entity']
+        description = row['Description']
+        logger.info("Validating the csv file of the table {}".format(table))
+        validate(path)
+        logger.info("Creating table={}, entity={}, description={}".format(table, entity, description))
+        create_table(path, entity, description)
+        logger.info("Creating columns for table={}".format(table))
+        create_atlan_columns(path)
+        # create_atlan_column_lineage(path, "DynamoDb")
+        logger.info("the job finished with success")
