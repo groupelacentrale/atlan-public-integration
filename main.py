22cd60be20139bfc22aaeb6d0822478f96e16ac1
Ismail Moussaoui <imoussaoui@lacentrale.fr>
2021-12-02 17:09:03 +0100

    adapt to the new convention

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import os
 
 import utils
+from TableDefinition import TableDefinition
 from atlanapi.atlanutils import AtlanSourceFile
 from validate_atlan_source_file import validate_atlan_source_file as validate
 from create_atlan_dynamodb_entity import create_atlan_dynamodb_entity as create_table
@@ -22,30 +23,27 @@ if __name__ == '__main__':
     tables_set = set()
     logger.info("******* Creating entities")
     for index, row in source_data.assets_def.iterrows():
-        table = row['Table']
-        path = utils.get_path(table)
+        table_name = row['Table']
+        integration_type = row['Integration Type']
         entity = row['Entity']
         description = row['Description']
-        logger.info("Creating table={}, entity={}, description={}".format(table, entity, description))
-        create_table(table, entity, description)
-        tables_set.add(table)
+        logger.info("Creating table={}, entity={}, description={}".format(table_name, entity, description))
+        create_table(table_name, entity, description)
+        tables_set.add(TableDefinition(table_name, integration_type))
 
     logger.info("******* Validating files")
     for table in tables_set:
-        path = utils.get_path(table)
-        logger.info("Validating the csv file of the table {}".format(table))
-        validate(path, 'DynamoDb')
+        logger.info("Validating the csv file of the table {}".format(table.name))
+        validate(table.path, table.integration_type)
 
     logger.info("******* Creating columns")
     for table in tables_set:
-        path = utils.get_path(table)
-        logger.info("Creating columns for table={}".format(table))
-        create_atlan_columns(path)
+        logger.info("Creating columns for table={}".format(table.name))
+        create_atlan_columns(table.path)
 
     logger.info("******* Creating lineage")
     for table in tables_set:
-        path = utils.get_path(table)
-        logger.info("Creating lineage for table={}".format(table))
-        create_atlan_column_lineage(path, "DynamoDb")
+        logger.info("Creating lineage for table={}".format(table.name))
+        create_atlan_column_lineage(table.path, "DynamoDb")
 
     logger.info("******* The job finished with success")
