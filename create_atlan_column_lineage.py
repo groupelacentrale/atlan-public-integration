b8d0b3de517f527761f43117ddef53adff37f5d3
Ismail Moussaoui <imoussaoui@lacentrale.fr>
2022-09-16 15:45:31 +0200

    add log for investigation

--- create_atlan_column_lineage.py
+++ create_atlan_column_lineage.py
@@ -104,6 +104,7 @@ def create_atlan_column_lineage(database_name, schema_name, table_or_entity_name
 
     logger.info("Creating entity lineage relationships for table: {}"
                 .format(get_csv_file_name(schema_name, table_or_entity_name, integration_type)))
+    logger.info("Entities lineage: {}".format(entities_lineage.values()))
     create_assets(entities_lineage.values())
 
 
