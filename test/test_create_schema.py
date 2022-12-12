5dd66a539441cd2115dca0e3df8b3f9393a38e30
mchen <mchen@lacentrale.fr>
2022-12-12 17:17:56 +0100

    DATACAT-123 : Test unitaires         + Ajout des tests unitaires adaptés

--- test/test_create_schema.py
+++ test/test_create_schema.py
@@ -1,35 +1,29 @@
 import json
+import logging
+from atlanapi.createAsset import create_assets
+from atlanapi.searchAssets import get_asset_guid_by_qualified_name
 from model.Asset import Schema
 from constants import INTEGRATION_TYPE_DYNAMO_DB
 
-TABLE = "test_table"
+logger = logging.getLogger("test_create_schema")
+DATABASE = "dynamo_db2"
 SCHEMA = "test_schema"
+TABLE = "test_table"
 INTEGRATION_TYPE = "DynamoDB"
 DESCRIPTION = "test schema atlan"
 TERM = "test_term"
 README = "test_schema readme"
 GLOSSARY = "test_schema glossary"
-DATA = {"entities": [
-    {
-        "typeName": "AtlanSchema",
-        "attributes": {
-            "typeName": "AtlanSchema",
-            "integrationType": INTEGRATION_TYPE,
-            "qualifiedName": "dynamodb/dynamodb.atlan.com/dynamo_db/{}".format(TABLE),
-            "name": TABLE,
-            "description": DESCRIPTION
-        }
-    }
-]}
 
 
 def test_create_schema():
-    schema = Schema(database_name=TABLE,
+    schema = Schema(database_name=DATABASE,
                     integration_type=INTEGRATION_TYPE_DYNAMO_DB,
-                    schema_name=TABLE,
+                    schema_name=SCHEMA,
                     description=DESCRIPTION,
                     readme=README,
                     term=TERM,
                     glossary=GLOSSARY)
-    s_payload = schema.get_creation_payload()
-    assert json.dumps(DATA) == s_payload
+    logger.info(schema.get_creation_payload())
+    create_assets([schema], "createSchemas")
+    assert get_asset_guid_by_qualified_name(schema.get_qualified_name(), "Schema") != {}
--- test/test_create_table.py
+++ test/test_create_table.py
@@ -1,39 +1,32 @@
 import json
+
+from atlanapi.createAsset import create_assets
+from atlanapi.searchAssets import get_asset_guid_by_qualified_name
 from model.Asset import Entity
 from constants import INTEGRATION_TYPE_DYNAMO_DB
 
+import logging
+
+logger = logging.getLogger()
+
 TABLE = "test_table"
-ENTITY = "test_entity"
 INTEGRATION_TYPE = "DynamoDb"
-DESCRIPTION = "Example Description"
-DATABASE = "test_database"
+DESCRIPTION = "Test Unit"
+DATABASE = "dynamo_db2"
 SCHEMA = "test_schema"
 README = "test_readme"
 TERM = "test_term"
 GLOSSARY = "test_glossary"
 
-DATA = {"entities": [
-    {
-        "typeName": "AtlanTable",
-        "attributes": {
-            "qualifiedName": "dynamodb/dynamodb.atlan.com/dynamo_db/{}/{}".format(TABLE, ENTITY),
-            "description": DESCRIPTION,
-            "name": ENTITY,
-            "integrationType": INTEGRATION_TYPE,
-            "typeName": "AtlanTable"
-        }
-    }
-]}
-
-
 def test_create_table():
-    schema = Entity(integration_type=INTEGRATION_TYPE_DYNAMO_DB,
-                    entity_name=ENTITY,
+    entity = Entity(integration_type=INTEGRATION_TYPE_DYNAMO_DB,
+                    entity_name=TABLE,
                     description=DESCRIPTION,
                     database_name=DATABASE,
                     schema_name=SCHEMA,
                     readme=README,
                     glossary=GLOSSARY,
                     term=TERM)
-    s_payload = schema.get_creation_payload()
-    assert json.dumps(DATA) == s_payload
+    create_assets([entity], "createTables")
+    logger.info(entity.get_creation_payload())
+    assert get_asset_guid_by_qualified_name(entity.get_qualified_name(), "Table") != {}
