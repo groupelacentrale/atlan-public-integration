b988c954cc32070504e280b1e418f2540d363b67
Marouane Felja <mfelja@lacentrale.fr>
2023-01-06 15:10:13 +0100

    DATACAT-123: cosmetics / preparing release

--- atlanapi/ApiConfig.py
+++ atlanapi/ApiConfig.py
@@ -8,6 +8,6 @@ class ApiConfig:
 
 
 def create_api_config():
-    atlan_api_token = os.environ.get('ATLAN_TOKEN')
-    atlan_instance = os.environ.get('ATLAN_NEW_INSTANCE')
+    atlan_api_token = os.environ.get('ATLAN_API_TOKEN')
+    atlan_instance = os.environ.get('ATLAN_INSTANCE')
     return ApiConfig(atlan_api_token, atlan_instance)
