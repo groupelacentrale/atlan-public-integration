2bb379563dc1829c1254e1080ceed836ae6daf06
mchen <mchen@lacentrale.fr>
2023-02-16 11:00:47 +0100

    DLH-879 : Unittests           + Ajout du script data.py, contient les constants pour les unittests           * Correction des unittests, schema, table, column et lineage suite aux modifications des différents assets

--- test/test_create_table.py
+++ test/test_create_table.py
@@ -4,96 +4,46 @@ import os
 from atlanapi.createAsset import create_assets, create_asset_database
 from atlanapi.delete_asset import delete_asset
 from atlanapi.searchAssets import get_asset_guid_by_qualified_name
-from model.Asset import Entity
-from constants import INTEGRATION_TYPE_DYNAMO_DB, INTEGRATION_TYPE_REDSHIFT, DYNAMODB_CONN_QN, INTEGRATION_TYPE_ATHENA, \
-    ATHENA_CONN_QN, REDSHIFT_CONN_QN
-
-import logging
-
-logger = logging.getLogger()
-
-DATABASE = "dynamo_db2"
-SCHEMA = "test_schema"
-ENTITY = "test_table"
-INTEGRATION_TYPE = INTEGRATION_TYPE_REDSHIFT
-DESCRIPTION = "Test integration schema test_schema"
-TERM = "test_term"
-README = "test_schema readme"
-GLOSSARY = "test_schema glossary"
-
-# Default connection qualified name is dynamo
-CONN_QN = DYNAMODB_CONN_QN
-CONN_NAME = INTEGRATION_TYPE_DYNAMO_DB.lower()
-
-if INTEGRATION_TYPE == INTEGRATION_TYPE_ATHENA:
-    CONN_NAME = ATHENA_CONN_QN
-    CONN_QN = ATHENA_CONN_QN + "/athena-tmp-test"
-elif INTEGRATION_TYPE == INTEGRATION_TYPE_REDSHIFT:
-    CONN_NAME = REDSHIFT_CONN_QN
-    CONN_QN = REDSHIFT_CONN_QN + "/redshift-tmp"
-
-DATA = {"entities": [
-    {
-        "typeName": "Table",
-        "attributes": {
-            "name": ENTITY,
-            "qualifiedName": "{}/{}/{}/{}".format(CONN_QN, DATABASE, SCHEMA, ENTITY),
-            "connectorName": CONN_NAME,
-            "schemaName": SCHEMA,
-            "schemaQualifiedName": "{}/{}/{}".format(CONN_QN, DATABASE, SCHEMA),
-            "databaseName": DATABASE,
-            "databaseQualifiedName": "{}/{}".format(CONN_QN, DATABASE),
-            "connectionQualifiedName": CONN_QN,
-            "description": DESCRIPTION
-        },
-        "relationshipAttributes": {
-            "atlanSchema": {
-                "typeName": "Schema",
-                "uniqueAttributes": {
-                    "qualifiedName": "{}/{}/{}".format(CONN_QN, DATABASE, SCHEMA)
-                }
-            }
-        }
-    }
-]}
+from model import Table
+from test.data import DATABASE, SCHEMA, ENTITY, INTEGRATION_TYPE, DESCRIPTION, README, TERM, GLOSSARY, TABLE_DATA
 
 
 def test_table_payload():
-    entity = Entity(integration_type=INTEGRATION_TYPE,
-                    entity_name=ENTITY,
-                    description=DESCRIPTION,
-                    database_name=DATABASE,
-                    schema_name=SCHEMA,
-                    readme=README,
-                    glossary=GLOSSARY,
-                    term=TERM)
+    entity = Table(integration_type=INTEGRATION_TYPE,
+                   entity_name=ENTITY,
+                   description=DESCRIPTION,
+                   database_name=DATABASE,
+                   schema_name=SCHEMA,
+                   readme=README,
+                   glossary=GLOSSARY,
+                   term=TERM)
     entity_payload = entity.get_creation_payload()
-    assert json.dumps(DATA) == entity_payload
+    assert json.dumps(TABLE_DATA) == entity_payload
 
 
 def test_create_table():
-    entity = Entity(integration_type=INTEGRATION_TYPE,
-                    entity_name=ENTITY,
-                    description=DESCRIPTION,
-                    database_name=DATABASE,
-                    schema_name=SCHEMA,
-                    readme=README,
-                    glossary=GLOSSARY,
-                    term=TERM)
+    entity = Table(integration_type=INTEGRATION_TYPE,
+                   entity_name=ENTITY,
+                   description=DESCRIPTION,
+                   database_name=DATABASE,
+                   schema_name=SCHEMA,
+                   readme=README,
+                   glossary=GLOSSARY,
+                   term=TERM)
     create_assets([entity], "createTables")
     entity_guid = get_asset_guid_by_qualified_name(entity.get_qualified_name(), "Table")
     assert delete_asset(entity_guid)
 
 
 def test_create_entity_database():
-    entity = Entity(integration_type=INTEGRATION_TYPE,
-                    entity_name=ENTITY,
-                    description=DESCRIPTION,
-                    database_name=DATABASE,
-                    schema_name=SCHEMA,
-                    readme=README,
-                    glossary=GLOSSARY,
-                    term=TERM)
+    entity = Table(integration_type=INTEGRATION_TYPE,
+                   entity_name=ENTITY,
+                   description=DESCRIPTION,
+                   database_name=DATABASE,
+                   schema_name=SCHEMA,
+                   readme=README,
+                   glossary=GLOSSARY,
+                   term=TERM)
     create_asset_database(entity)
     schema_qualified_name = os.path.split(entity.get_qualified_name())[0]
     database_qualified_name = os.path.split(schema_qualified_name)[0]
--- test/test_lineage.py
+++ test/test_lineage.py
@@ -1,143 +1,54 @@
 import json
 
-from constants import DYNAMODB_CONN_QN, INTEGRATION_TYPE_DYNAMO_DB, INTEGRATION_TYPE_ATHENA, ATHENA_CONN_QN, \
-    INTEGRATION_TYPE_REDSHIFT, REDSHIFT_CONN_QN
 from model import TableLineage, ColumnLineage, Table, Column
+from test.data import DATABASE, SCHEMA, ENTITY, ENTITY_OUT, COLUMN, COLUMNOP, INTEGRATION_TYPE, DESCRIPTION, DATA_TYPE, \
+    TABLE_LINEAGE_DATA, COLUMN_LINEAGE_DATA
 
-COLUMN = "col_index"
-COLUMNOP = "col_index_output"
-ENTITY = "test_table"
-ENTITY_OUT = "test_table_out"
-INTEGRATION_TYPE = INTEGRATION_TYPE_DYNAMO_DB
-DESCRIPTION = "Test Unit"
-DATABASE = "dynamo_db2"
-DATATYPE = "INT"
-SCHEMA = "test_schema"
 TARGET = "Target"
 SOURCE = "SOURCE"
-
-#Default connection qualified name is dynamo
-CONN_QN = DYNAMODB_CONN_QN
-CONN_NAME = INTEGRATION_TYPE_DYNAMO_DB.lower()
-
-if INTEGRATION_TYPE == INTEGRATION_TYPE_ATHENA:
-    CONN_NAME = ATHENA_CONN_QN
-    CONN_QN = ATHENA_CONN_QN + "/athena-tmp-test"
-elif INTEGRATION_TYPE == INTEGRATION_TYPE_REDSHIFT:
-    CONN_NAME = REDSHIFT_CONN_QN
-    CONN_QN = REDSHIFT_CONN_QN + "/redshift-tmp"
-
-IN_PROC = "{}/{}/{}/{}".format(CONN_QN, DATABASE, SCHEMA, ENTITY)
-OUT_PROC = "{}/{}/{}/{}".format(CONN_QN, DATABASE, SCHEMA, ENTITY_OUT)
-
-IN_COL_PROC = "{}/{}/{}/{}/{}".format(CONN_QN, DATABASE, SCHEMA, ENTITY, COLUMN)
-OUT_COL_PROC = "{}/{}/{}/{}/{}".format(CONN_QN, DATABASE, SCHEMA, ENTITY_OUT, COLUMNOP)
-
-DATA_PROCESS = {
-    "typeName": "Process",
-    "attributes": {
-        "name": "{}-{} Transformation".format(INTEGRATION_TYPE, INTEGRATION_TYPE),
-        "qualifiedName": "{}/{}".format(IN_PROC, OUT_PROC),
-        "connectorName": CONN_NAME,
-        "connectionQualifiedName": CONN_QN
-    },
-    "relationshipAttributes": {
-        "inputs": [
-            {
-                "typeName": "Table",
-                "uniqueAttributes": {
-                    "qualifiedName": IN_PROC
-                }
-            }
-        ],
-        "outputs": [
-            {
-                "typeName": "Table",
-                "uniqueAttributes": {
-                    "qualifiedName": OUT_PROC
-                }
-            }
-        ]
-    }
-}
-
-DATA_COLUMN_PROCESS = {
-    "typeName": "ColumnProcess",
-    "attributes": {
-        "name": "{}-{} Transformation".format(INTEGRATION_TYPE, INTEGRATION_TYPE),
-        "qualifiedName": "{}/{}".format(IN_COL_PROC, OUT_COL_PROC),
-        "connectorName": CONN_NAME,
-        "connectionQualifiedName": CONN_QN
-    },
-    "relationshipAttributes": {
-        "inputs": [
-            {
-                "typeName": "Column",
-                "uniqueAttributes": {
-                    "qualifiedName": IN_COL_PROC
-                }
-            }
-        ],
-        "outputs": [
-            {
-                "typeName": "Column",
-                "uniqueAttributes": {
-                    "qualifiedName": OUT_COL_PROC
-                }
-            }
-        ],
-        "process": {
-            "typeName": "Process",
-            "uniqueAttributes": {
-                "qualifiedName": "{}/{}".format(IN_PROC, OUT_PROC)
-            }
-        }
-    }
-}
 
 
 def test_process_lineage_payload():
     entity_src = Table(integration_type=INTEGRATION_TYPE,
-                        entity_name=ENTITY,
-                        description=DESCRIPTION,
-                        database_name=DATABASE,
-                        schema_name=SCHEMA
-                        )
+                       entity_name=ENTITY,
+                       description=DESCRIPTION,
+                       database_name=DATABASE,
+                       schema_name=SCHEMA
+                       )
 
     entity_target = Table(integration_type=INTEGRATION_TYPE,
-                           entity_name=ENTITY_OUT,
-                           description=DESCRIPTION,
-                           database_name=DATABASE,
-                           schema_name=SCHEMA
+                          entity_name=ENTITY_OUT,
+                          description=DESCRIPTION,
+                          database_name=DATABASE,
+                          schema_name=SCHEMA
+                          )
+
+    process = TableLineage(table=entity_src,
+                           lineage_type=TARGET,
+                           lineage_integration_type=INTEGRATION_TYPE,
+                           lineage_database_name=entity_target.database_name,
+                           lineage_schema_name=entity_target.schema_name,
+                           lineage_table_name=entity_target.entity_name,
+                           lineage_full_qualified_name=entity_target.get_qualified_name()
                            )
 
-    process = TableLineage(entity=entity_src,
-                            lineage_type=TARGET,
-                            lineage_integration_type=INTEGRATION_TYPE,
-                            lineage_database_name=entity_target.database_name,
-                            lineage_schema_name=entity_target.schema_name,
-                            lineage_entity_name=entity_target.entity_name,
-                            lineage_full_qualified_name=entity_target.get_qualified_name()
-                            )
-
-    assert json.dumps(process.get_creation_payload_for_bulk_mode()) == json.dumps(DATA_PROCESS)
+    assert json.dumps(process.get_creation_payload_for_bulk_mode()) == json.dumps(TABLE_LINEAGE_DATA)
 
 
 def test_column_lineage_payload():
-
     column_inp = Column(integration_type=INTEGRATION_TYPE,
                         database_name=DATABASE,
                         schema_name=SCHEMA,
                         entity_name=ENTITY,
                         column_name=COLUMN,
-                        data_type=DATATYPE,
+                        data_type=DATA_TYPE,
                         )
     column_op = Column(integration_type=INTEGRATION_TYPE,
                        database_name=DATABASE,
                        schema_name=SCHEMA,
                        entity_name=ENTITY_OUT,
                        column_name=COLUMNOP,
-                       data_type=DATATYPE,
+                       data_type=DATA_TYPE,
                        )
     col_lineage = ColumnLineage(column_inp,
                                 lineage_type=TARGET,
@@ -147,4 +58,4 @@ def test_column_lineage_payload():
                                 lineage_column_name=column_op.column_name,
                                 lineage_full_qualified_name=column_op.get_qualified_name())
 
-    assert json.dumps(col_lineage.get_creation_payload_for_bulk_mode()) == json.dumps(DATA_COLUMN_PROCESS)
+    assert json.dumps(col_lineage.get_creation_payload_for_bulk_mode()) == json.dumps(COLUMN_LINEAGE_DATA)
