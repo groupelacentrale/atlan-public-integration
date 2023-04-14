ab49847d56ab79efeac56deed532eb2e7c7367b5
MikkySupreme <mikky.chn@gmail.com>
2023-04-14 15:56:07 +0200

    DLH-945 : Réparation de la suppression de colonne
        * Correction de la fonction get_entity_columns
              - L'objet obtenu par la requête était mal traité, d'où les erreurs id

--- atlanapi/get_entity_columns.py
+++ atlanapi/get_entity_columns.py
@@ -16,15 +16,16 @@ search_headers = {
 
 
 def get_entity_columns(entity_guid):
-    search_url = "https://{}//api/meta/entity/guid/{}".format(api_conf.instance, entity_guid)
-    atlan_api_query_request_object = AtlanApiRequest("GET", search_url, search_headers, {})
     try:
-        columns_response = json.loads(atlan_api_query_request_object.send_atlan_request().text)
-        columns = columns_response['entity']['relationshipAttributes']['columns']
+        search_url = "https://{}/api/meta/entity/guid/{}".format(api_conf.instance, entity_guid)
+        atlan_api_query_request_object = AtlanApiRequest("GET", search_url, search_headers, None)
+        response = atlan_api_query_request_object.send_atlan_request()
+        response_object = response.json()
+        columns = response_object['entity']['relationshipAttributes']['columns']
         if columns:
             return {column['displayText']: column['guid'] for column in columns}
         # Not really an error, because it is normal that an entity doesn't have columns at first.
-        logger.debug("entity '{}' does not have any columns".format(entity_guid))
+        logger.info("entity '{}' does not have any columns".format(entity_guid))
         return {}
     except:
         logger.error("Error while fetching columns for entity id '{}'".format(entity_guid))
