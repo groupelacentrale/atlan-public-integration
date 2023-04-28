77036293b5d4b5d1b98ad83d185e67f5bcfc2555
Marouane Felja <mfelja@lacentrale.fr>
2023-04-28 10:31:14 +0200

    fix: make classification and criticality optionnal to prevent breaking integration

--- create_atlan_schema_and_entities.py
+++ create_atlan_schema_and_entities.py
@@ -37,8 +37,8 @@ def create_atlan_schema_and_entities(path_to_manifest, sep=","):
                           readme=row['Readme'],
                           term=row['Term'],
                           glossary=row['Glossary'],
-                          classification=row['Classification'],
-                          criticality=row['Criticality'],
+                          classification=row['Classification'] if 'Classification' in row.keys() else None,
+                          criticality=row['Criticality'] if 'Criticality' in row.keys() else None,
                           integration_type=row['Integration Type'])
             tables.append(table)
             # Create schema from entity row in case schema row is missing
