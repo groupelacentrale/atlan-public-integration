a48d60d3872fb1666f480a5497d8be6fe4c342cc
glc-mchen <mchen@lacentrale.fr>
2024-08-07 15:01:33 +0200

    DATACAT-297: Moulinette 2.0
    Check for classification tag and criticality

--- atlanapi/attach_classification.py
+++ atlanapi/attach_classification.py
@@ -52,8 +52,9 @@ def attach_classification(assets):
         detach_classification(assets_with_classification)
         for asset in assets_with_classification:
             payload = json.dumps(list(asset.get_classification_payload()))
-            attach_classification_url = 'https://{}/api/meta/entity/bulk/classification/displayName'.format(
-                api_conf.instance)
+            attach_classification_url = ('https://{}/api/meta/entity/uniqueAttribute/'
+                                         'type/{}/classifications?attr:qualifiedName={}').format(
+                api_conf.instance, asset.get_atlan_type_name(), asset.get_qualified_name())
             atlan_api_request_object = AtlanApiRequest("POST", attach_classification_url, headers, payload)
             response = atlan_api_request_object.send_atlan_request()
             logger.info("Attach classification for assets : {} {} - tag {}, response {}".format(asset.get_atlan_type_name(), asset.get_asset_name(), asset.classification, response.status_code))
